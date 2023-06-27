fdd036f02f9f837746dd6fbb95e9418590d9905a
ThrawnK <kevinclairvoyantz@icloud.com>
2023-06-27 12:31:40 +0800

    Updated parameter description

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,11 @@
 export = fetcher;
 
-declare function fetcher(url: string, method: "GET" | "POST", headers: Record<string, string>, body?: Record<string, any>):Promise<any>;
+/**
+ * 
+ * @param url The URL to use http(s) GET or POST
+ * @param method Either "GET" or "POST"
+ * @param headers Pairs of string: string objects
+ * @param body same as headers
+ * @returns the object delivered by the fetch request, but already parsed as object. Returns the error if occured.
+ */
+declare function fetcher(url: string, method: "GET" | "POST", headers: Record<string, string>, body?: Record<string, any>): Promise<any>;
